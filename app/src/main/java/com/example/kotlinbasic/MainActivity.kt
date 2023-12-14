bb18cc2fdb8ddce7a5a8c6bfd52d5229e2c9cfb0
fahmi <fahmiabdulaziz44@gmail.com>
2023-12-14 20:33:47 +0700

    pending

--- app/src/main/java/com/example/kotlinbasic/MainActivity.kt
+++ app/src/main/java/com/example/kotlinbasic/MainActivity.kt
@@ -10,22 +10,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        var btn = findViewById<Button>(R.id.btn)
-        var btn2 = findViewById<Button>(R.id.btn2)
-
-        btn.setOnClickListener {
-            // Explicit intent
-            var intent = Intent(this,MainActivity2::class.java)
-            intent.putExtra("age",25)
-            startActivity(intent)
-        }
-
-        btn2.setOnClickListener {
-            // Implicit intent
-            var intent = Intent(Intent.ACTION_VIEW)
-            intent.data = Uri.parse("http://www.google.com")
-            startActivity(intent)
-        }
     }
 }
