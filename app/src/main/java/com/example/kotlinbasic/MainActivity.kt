cc9de8a6e49d1053fa0b944abc59c7c0de8ec2f5
fahmi <fahmiabdulaziz44@gmail.com>
2023-12-18 20:31:22 +0700

    simple array adapter

--- app/src/main/java/com/example/kotlinbasic/MainActivity.kt
+++ app/src/main/java/com/example/kotlinbasic/MainActivity.kt
@@ -6,8 +6,10 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.ListView
 import android.widget.SeekBar
 import android.widget.TextView
 import android.widget.Toast
@@ -15,110 +17,28 @@ import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
 
-    private val handler: Handler?=null
-    private lateinit var mediaPlayer: MediaPlayer
-    private lateinit var titleText: TextView
-    private lateinit var timeText: TextView
-    private lateinit var seekBar: SeekBar
-    private lateinit var playBtn: ImageButton
-    private lateinit var stopBtn: ImageButton
-    private lateinit var forwardBtn: ImageButton
-    private lateinit var rewindBtn: ImageButton
-    private var finalTime: Double = 0.0
-    private var startTime: Double = 0.0
-    private var forwardTime: Int = 10000
-    private var backwardTime: Int = 10000
-    private var oneTimeOnly: Int = 0
+    private lateinit var listView: ListView
+    private lateinit var arrayAdapter: ArrayAdapter<String>
+    private var worldCups: ArrayList<String> = arrayListOf(
+        "Indonesia",
+        "Malaysia",
+        "Thailand",
+        "Vietnam"
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-         titleText = findViewById(R.id.txtNama)
-         timeText = findViewById(R.id.txtTime)
-         seekBar = findViewById(R.id.seekBar)
-         playBtn = findViewById(R.id.imgPlay)
-         stopBtn = findViewById(R.id.imgPlay)
-         forwardBtn = findViewById(R.id.imgFastForward)
-        rewindBtn = findViewById(R.id.imgFastRewind)
+        listView = findViewById(R.id.listView)
 
-        // Media Player
-        mediaPlayer = MediaPlayer.create(this,R.raw.fix_you)
+        arrayAdapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_list_item_1,
+            worldCups,
+        )
 
-        // un click seekbar
-        seekBar.isClickable = false
-
-        // play btn
-        playBtn.setOnClickListener {
-            mediaPlayer.start()
-
-            finalTime = mediaPlayer.duration.toDouble()
-            startTime = mediaPlayer.currentPosition.toDouble()
-
-            if(oneTimeOnly == 0){
-                seekBar.max = finalTime.toInt()
-                oneTimeOnly = 1
-            }
-
-            timeText.text = startTime.toString()
-            seekBar.progress = startTime.toInt()
-
-            handler?.postDelayed(updateSongTime,100)
-        }
-
-        // stop btn
-        stopBtn.setOnClickListener {
-            mediaPlayer.pause()
-        }
-
-        // forward btn
-        forwardBtn.setOnClickListener {
-            var temp = startTime
-            if(temp + forwardTime <= finalTime){
-                startTime+=forwardTime
-                mediaPlayer.seekTo(startTime.toInt())
-            }else{
-                Toast.makeText(this, "Cannot jump forward", Toast.LENGTH_SHORT).show()
-            }
-        }
-
-        // rewind btn
-        rewindBtn.setOnClickListener {
-            var temp = startTime
-            if(temp - backwardTime > 0){
-                startTime-=backwardTime
-                mediaPlayer.seekTo(startTime.toInt())
-            }else{
-                Toast.makeText(this, "Cannot jump rewind", Toast.LENGTH_SHORT).show()
-            }
-        }
-
-
-        titleText.text = resources.getIdentifier("fix_you","raw",packageName).toString()
-    }
-
-    private val updateSongTime: Runnable = object : Runnable{
-        override fun run() {
-            startTime = mediaPlayer.currentPosition.toDouble()
-
-            timeText.text = String.format(
-                "%d min, %d sec",
-                TimeUnit.MILLISECONDS.toMinutes(
-                    startTime.toLong()
-                ),
-                TimeUnit.MILLISECONDS.toSeconds(
-                    startTime.toLong() - TimeUnit.MINUTES.toSeconds(
-                        TimeUnit.MILLISECONDS.toMinutes(
-                            startTime.toLong()
-                        )
-                    )
-                ),
-            )
-
-            seekBar.progress = startTime.toInt()
-
-            handler?.postDelayed(this,100)
-        }
+        listView.adapter = arrayAdapter
 
     }
 }
