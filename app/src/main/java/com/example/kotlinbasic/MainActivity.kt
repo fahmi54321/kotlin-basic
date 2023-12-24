80a9cf95c2e3e8d324afd41aa770f5688363f9f3
fahmi <fahmiabdulaziz44@gmail.com>
2023-12-24 15:56:45 +0700

    tab layout

--- app/src/main/java/com/example/kotlinbasic/MainActivity.kt
+++ app/src/main/java/com/example/kotlinbasic/MainActivity.kt
@@ -11,21 +11,30 @@ import androidx.fragment.app.FragmentTransaction
 import androidx.viewpager2.widget.ViewPager2
 import com.example.kotlinbasic.adapter.MyAdapter
 import com.google.android.material.navigation.NavigationView
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var myAdapter: MyAdapter
-    private lateinit var arrayList: ArrayList<String>
+    private val tabsArray = arrayOf("One","Two","Three")
     private lateinit var viewPager2: ViewPager2
+    private lateinit var tabLayout: TabLayout
+    private lateinit var myAdapter: MyAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        viewPager2 = findViewById(R.id.viewPager2)
-        myAdapter = MyAdapter(this)
+        viewPager2 = findViewById(R.id.viewPager)
+        tabLayout = findViewById(R.id.tabLayout)
+        myAdapter = MyAdapter(supportFragmentManager,lifecycle)
 
-        viewPager2.orientation = ViewPager2.ORIENTATION_HORIZONTAL
         viewPager2.adapter = myAdapter
+
+        TabLayoutMediator(tabLayout,viewPager2){
+            tab,position -> tab.text = tabsArray[position]
+        }.attach()
+
+
     }
 }
--- app/src/main/java/com/example/kotlinbasic/adapter/MyAdapter.kt
+++ app/src/main/java/com/example/kotlinbasic/adapter/MyAdapter.kt
@@ -1,13 +1,15 @@
 package com.example.kotlinbasic.adapter
 
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.kotlinbasic.MainActivity
 import com.example.kotlinbasic.fragment.FragmentOne
 import com.example.kotlinbasic.fragment.FragmentThree
 import com.example.kotlinbasic.fragment.FragmentTwo
 
-class MyAdapter(private val mainActivity: MainActivity) : FragmentStateAdapter(mainActivity) {
+class MyAdapter(private val fragmentManager: FragmentManager,lifecycle: Lifecycle) : FragmentStateAdapter(fragmentManager, lifecycle) {
     override fun getItemCount(): Int {
         return 3
     }
