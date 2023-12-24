2f4a1c42082bb0bf314655ed8d473d2624e0450d
fahmi <fahmiabdulaziz44@gmail.com>
2023-12-24 16:27:26 +0700

    shared preferences

--- app/src/main/java/com/example/kotlinbasic/MainActivity.kt
+++ app/src/main/java/com/example/kotlinbasic/MainActivity.kt
@@ -1,40 +1,49 @@
 package com.example.kotlinbasic
 
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MenuItem
 import android.widget.Button
-import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentTransaction
-import androidx.viewpager2.widget.ViewPager2
-import com.example.kotlinbasic.adapter.MyAdapter
-import com.google.android.material.navigation.NavigationView
-import com.google.android.material.tabs.TabLayout
-import com.google.android.material.tabs.TabLayoutMediator
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-    private val tabsArray = arrayOf("One","Two","Three")
-    private lateinit var viewPager2: ViewPager2
-    private lateinit var tabLayout: TabLayout
-    private lateinit var myAdapter: MyAdapter
+    private lateinit var edtUsername: EditText
+    private lateinit var txtUsername: TextView
+    private lateinit var btnSimpan: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        viewPager2 = findViewById(R.id.viewPager)
-        tabLayout = findViewById(R.id.tabLayout)
-        myAdapter = MyAdapter(supportFragmentManager,lifecycle)
+        edtUsername = findViewById(R.id.edtUsername)
+        txtUsername = findViewById(R.id.txtLastUserName)
+        btnSimpan = findViewById(R.id.btnSimpan)
 
-        viewPager2.adapter = myAdapter
+        displayName()
 
-        TabLayoutMediator(tabLayout,viewPager2){
-            tab,position -> tab.text = tabsArray[position]
-        }.attach()
+        btnSimpan.setOnClickListener {
+            val edtName = edtUsername.text.toString()
 
+            saveNameInSharedPref(edtName)
+        }
 
+    }
+
+    private fun saveNameInSharedPref(edtName: String){
+        val sharedPreferences: SharedPreferences = getSharedPreferences("username", MODE_PRIVATE)
+
+        val editor: SharedPreferences.Editor = sharedPreferences.edit()
+
+        editor.putString("name",edtName)
+        editor.commit()
+    }
+
+    private fun displayName(){
+        val sharedPreferences: SharedPreferences = getSharedPreferences("username", MODE_PRIVATE)
+        val data = sharedPreferences.getString("name","")
+
+        txtUsername.text = data
     }
 }
